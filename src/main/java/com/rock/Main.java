cf83b91ff2c018fb8972f14fdda03bba4fce726f
Rock-Ayl <AnYongLiang4869@163.com>
2026-02-25 22:15:00 +0800

    新增：Maven配置、启动方法

---
+++ src/main/java/com/rock/Main.java
@@ -0,0 +1,17 @@
+package com.rock;
+
+/**
+ * 启动器
+ */
+public class Main {
+
+    /**
+     * 一切工作流程,从这里开始
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
