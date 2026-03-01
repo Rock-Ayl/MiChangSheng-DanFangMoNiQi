c71958661c318a50abe5dcf60b933b2d531b7c0c
Rock-Ayl <AnYongLiang4869@163.com>
2026-03-01 21:12:18 +0800

    修改：丹炉-3

--- src/main/java/com/rock/enums/DanLuEnum.java
+++ src/main/java/com/rock/enums/DanLuEnum.java
@@ -10,14 +10,14 @@ import lombok.ToString;
 @ToString
 public enum DanLuEnum {
 
-    NONE("none", GroupEnum.NONE, 0),
+    NONE("none", GroupEnum.NONE, 0, false, false),
 
-    ONE("一品丹炉", GroupEnum.ONE, 9),
-    TWO("二品丹炉", GroupEnum.TWO, 10),
-    THREE("三品丹炉", GroupEnum.THREE, 11),
-    FOUR("四品丹炉", GroupEnum.FOUR, 12),
-    FIVE("五品丹炉", GroupEnum.FIVE, 13),
-    SIX("六品丹炉", GroupEnum.SIX, 14),
+    ONE("一品丹炉", GroupEnum.ONE, 9, false, false),
+    TWO("二品丹炉", GroupEnum.TWO, 10, false, true),
+    THREE("三品丹炉", GroupEnum.THREE, 11, false, true),
+    FOUR("四品丹炉", GroupEnum.FOUR, 12, false, true),
+    FIVE("五品丹炉", GroupEnum.FIVE, 13, false, true),
+    SIX("六品丹炉", GroupEnum.SIX, 14, true, true),
 
     ;
 
@@ -30,11 +30,19 @@ public enum DanLuEnum {
     //最大草药数
     private final Integer maxCount;
 
+    //是否有主药2槽位
+    private final boolean main2;
+
+    //是否有辅药2槽位
+    private final boolean secondary2;
+
     //初始化
-    DanLuEnum(String code, GroupEnum groupEnum, Integer maxCount) {
+    DanLuEnum(String code, GroupEnum groupEnum, Integer maxCount, boolean main2, boolean secondary2) {
         this.code = code;
         this.groupEnum = groupEnum;
         this.maxCount = maxCount;
+        this.main2 = main2;
+        this.secondary2 = secondary2;
     }
 
 }
