40646b4ff543b4a55e92ab82775143d8ccbdf73e
Rock-Ayl <AnYongLiang4869@163.com>
2026-03-05 11:57:10 +0800

    修改：输出单方-4

---
+++ src/main/java/com/rock/Config.java
@@ -0,0 +1,17 @@
+package com.rock;
+
+/**
+ * 配置类
+ */
+public class Config {
+
+    //当单方没有主药2,是否将其用于平衡寒热(会大幅增加计算时间)
+    public static final boolean SWITCH_MAIN_1_GUIDE = false;
+
+    //当单方没有辅药1,是否将其用于平衡寒热(会大幅增加计算时间)
+    public static final boolean SWITCH_SEC_1_GUIDE = false;
+
+    //当单方没有辅药2,是否将妻用于平衡寒热(会大幅增加计算时间)
+    public static final boolean SWITCH_SEC_2_GUIDE = false;
+
+}
