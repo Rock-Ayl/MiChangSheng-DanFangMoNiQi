eda48170655b1bff8e586b9ac0be9477b637e604
Rock-Ayl <AnYongLiang4869@163.com>
2026-03-05 22:40:35 +0800

    修改：输出单方-32

--- src/main/java/com/rock/Config.java
+++ src/main/java/com/rock/Config.java
@@ -17,7 +17,7 @@ public class Config {
      */
 
     //药材是否包含妖丹
-    public static final boolean NEED_YAO_DAN = false;
+    public static final boolean NEED_YAO_DAN = true;
 
     /**
      * 开关-额外平衡寒热
