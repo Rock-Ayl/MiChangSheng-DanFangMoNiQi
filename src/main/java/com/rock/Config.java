57a92580d2ae2fd2b5c966afd82f715221fe2780
Rock-Ayl <AnYongLiang4869@163.com>
2026-03-08 18:15:47 +0800

    修改：宗门库房逻辑-28

--- src/main/java/com/rock/Config.java
+++ src/main/java/com/rock/Config.java
@@ -24,6 +24,8 @@ public class Config {
     public static final String FILE_PATH_ZONG_MEN_ZHU_SHAN_FILE = "/觅长生-药材-竹山宗库房.txt";
     //药材配置-宗门库房-离火
     public static final String FILE_PATH_ZONG_MEN_LI_HUO_FILE = "/觅长生-药材-离火门库房.txt";
+    //药材配置-宗门库房-化尘
+    public static final String FILE_PATH_ZONG_MEN_IZHAN_FILE = "/觅长生-药材-化尘教库房.txt";
 
     //丹药配置-所有
     public static final String FILE_PATH_ALL_PILL_FILE = "/觅长生-丹药.txt";
@@ -33,7 +35,7 @@ public class Config {
      */
 
     //开关-使用哪种-药材配置-作为药材组合单方
-    public static final String SWITCH_YAO_CAI_FILE_PATH = FILE_PATH_ZONG_MEN_LI_HUO_FILE;
+    public static final String SWITCH_YAO_CAI_FILE_PATH = FILE_PATH_ZONG_MEN_IZHAN_FILE;
 
     //药材是否包含妖丹
     public static final boolean NEED_YAO_DAN = true;
