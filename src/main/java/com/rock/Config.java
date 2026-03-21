e1bdc829e0fef9ed6a313c6687c05a36437fd373
Rock-Ayl <AnYongLiang4869@163.com>
2026-03-21 16:42:42 +0800

    修改：一品药材【填充物平衡寒热】-4

--- src/main/java/com/rock/Config.java
+++ src/main/java/com/rock/Config.java
@@ -47,7 +47,7 @@ public class Config {
     //开关-使用哪种-药材配置-作为药材组合单方
     public static final String SWITCH_YAO_CAI_FILE_PATH = FILE_PATH_ZONG_MEN_XIANG_HUA_FILE;
 
-    //药材是否包含妖丹
+    //使用的药材是否包含妖丹
     public static final boolean NEED_YAO_DAN = true;
 
     /**
